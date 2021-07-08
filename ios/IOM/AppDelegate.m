a28c261afff69ef2fed77b82f9eb27567e35607d
Jose Hurtado <57419008+jhurtadog@users.noreply.github.com>
2021-07-08 17:51:12 -0500

    update from cetrivinom (Apple)

--- ios/IOM/AppDelegate.m
+++ ios/IOM/AppDelegate.m
@@ -4,6 +4,7 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 @import Firebase;
+#import <GoogleMaps/GoogleMaps.h>
 
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
@@ -48,6 +49,7 @@ static void InitializeFlipper(UIApplication *application) {
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  [GMSServices provideAPIKey:@"AIzaSyDNsajpMtzfkHWk-CFeNnMbnlatvlCsXls"];
   [FIRApp configure];
   return YES;
 }
